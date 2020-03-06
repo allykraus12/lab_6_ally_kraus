202a13fefcea25a790d12e6f95ac9e13a97a5b6c
Ally Kraus <allykraus@Allys-Air.fios-router.home>
2020-03-05 22:09:07 -0500

    work in progress

---
+++ lab6problem1.py
@@ -0,0 +1,25 @@
+class Node:
+	def __init_(self, data, nextPointer):
+
+		self.data        = data
+		self.nextPointer = nextPointer
+
+	def getData():
+
+		return data
+
+	def getNextPointer():
+
+		return nextPointer
+		
+	def setData(self,data):
+
+		self.data = data
+
+	def setNextPointer(self, nextPointer):
+
+		self.nextPointer = nextPointer
+
+
+
+
--- lab6problem2.py
+++ lab6problem2.py
@@ -1,28 +1,54 @@
-from init import Node
+from lab6problem1 import Node
 
 class LinkedList:
-	def __init_(self, head, size, tail):
+	def __init__(self, head, tail):
 		self.head = head 
 		self.tail = tail 
 
 	def getHead(self,head):
 
-		self.head = head 
+		self.head == head 
 
-	def getSize():
+	def getSize(self):
 
-		return null
+		nodeCount = 0
+		currentNode = head
+		while True:
+			if currentNode.nextPointer is None:
+			    return nodeCount         
+        	currentNode = currentNode.nextPointer
+        	nodeCount = nodeCount + 1
 
-	def getTail():
+        return nodeCount
+
+	def getTail(self):
 		
 		self.tail = tail
+
  	def addNode(self,data):
  		nodeVariable = Node()
  		nodeVariable.setData(data)
 
+ 		if self.getSize()==0:
+ 			head = nodeVariable
 
-		
-	
+def main():
+
+	linkedList1 = LinkedList(head=None, tail=None)
+
+
+
+	print(linkedList1.getSize())
+
+	print(linkedList1.addNode("data1"))
+
+	print(linkedList1.getSize())
+
+
+
+
+if __name__ == '__main__':
+	main()
 
 
 	
