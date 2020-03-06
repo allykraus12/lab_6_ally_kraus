7e6ee53c7ced48ffc57c9f10911da61f768d3e89
Ally Kraus <allykraus@Allys-Air.fios-router.home>
2020-03-05 20:48:09 -0500

    “Init
    git commit -m “Init

---
+++ lab6problem2.py
@@ -0,0 +1,28 @@
+from init import Node
+
+class LinkedList:
+	def __init_(self, head, size, tail):
+		self.head = head 
+		self.tail = tail 
+
+	def getHead(self,head):
+
+		self.head = head 
+
+	def getSize():
+
+		return null
+
+	def getTail():
+		
+		self.tail = tail
+ 	def addNode(self,data):
+ 		nodeVariable = Node()
+ 		nodeVariable.setData(data)
+
+
+		
+	
+
+
+	
